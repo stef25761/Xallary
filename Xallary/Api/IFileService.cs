a6eb61220d3e6dca01c83817dca6f4feccf0880d
Amasuke <Amasuke@users.noreply.github.com>
2021-05-06 14:16:41 +0200

    Added API Files
    Unnecessary to make 2 almost empty files a task

---
+++ Xallary/Api/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Xallary.Api
+{
+    public interface IFileService
+    {
+        void SavePicture(string name, byte[] photoData);
+    }
+}
